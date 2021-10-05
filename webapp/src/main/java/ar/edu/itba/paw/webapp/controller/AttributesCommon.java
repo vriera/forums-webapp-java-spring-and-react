f3340bffe830388e5694b8a6af05da7ce78c1369
Natu <nlilienthal@itba.edu.ar>
2021-10-05 19:38:59 -0300

    AtributesCommon

---
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/AttributesCommon.java
@@ -0,0 +1,23 @@
+package ar.edu.itba.paw.webapp.controller;
+
+import ar.edu.itba.paw.interfaces.services.UserService;
+import ar.edu.itba.paw.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+public class AttributesCommon {
+
+    @Autowired
+    private UserService us;
+
+    @ModelAttribute("currentUser")
+    public User currentUser() {
+        final Optional<User> user = us.findByEmail(SecurityContextHolder.getContext().getAuthentication().getName());
+        return user.isPresent() ? user.get() : null;
+    }
+}
