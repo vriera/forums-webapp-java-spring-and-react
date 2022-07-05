6ab240848b486b81854b77d64f841272b1430890
Salus Zavalía <szavalia@itba.edu.ar>
2022-07-05 01:39:22 -0300

    Added i18n, started on profile and navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
-
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import AskPage from "./pages/question/ask";
 import LandingPage from "./pages/landing";
-
-
+import ProfilePage from "./pages/dashboard/profile";
 
 function App(){
     return (
@@ -13,10 +11,10 @@ function App(){
                 <Routes>
                     <Route path="/" element={<LandingPage/>} />
                     <Route path="/ask" element={<AskPage/>} />
+                    <Route path="/dashboard/profile" element={<ProfilePage/>} />
                 </Routes>
             </Router>
         </div>
-
     )
 }
 
---
+++ frontend/src/pages/dashboard/profile.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Navbar from "./../../components/navbar";
+import '../../resources/styles/argon-design-system.css';
+import '../../resources/styles/blk-design-system.css';
+import '../../resources/styles/general.css';
+import '../../resources/styles/stepper.css';
+
+
+const ProfilePage = () => {
+    return (
+        <div>
+            < Navbar />
+            <p> HOLA MUNDO !</p>
+        </div>
+    );
+};
+
+export default ProfilePage;
