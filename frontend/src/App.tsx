8bcfa9c8def079585cec610b79b68b04c489e581
Salus Zavalía <szavalia@itba.edu.ar>
2022-07-05 14:43:34 -0300

    Added updateProfile page, lacks input control

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import AskPage from "./pages/question/ask";
 import LandingPage from "./pages/landing";
-import Profile from "./pages/dashboard/profile";
+import ProfilePage from "./pages/dashboard/user/profile";
+import UpdateProfilePage from "./pages/dashboard/user/updateProfile"
 
 
 function App(){
@@ -12,7 +13,9 @@ function App(){
                 <Routes>
                     <Route path="/" element={<LandingPage/>} />
                     <Route path="/ask" element={<AskPage/>} />
-                    <Route path="/dashboard/profile" element={<Profile/>} />
+                    <Route path="/dashboard/profile" element={<ProfilePage/>} />
+                    <Route path="/dashboard/user/updateProfile" element={<UpdateProfilePage/>} />
+
                 </Routes>
             </Router>
         </div>
