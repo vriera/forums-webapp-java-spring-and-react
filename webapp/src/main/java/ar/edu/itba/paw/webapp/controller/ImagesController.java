24f1cea2ccd0561a07d0cf45b3741fd97aa3d24f
Natu <nlilienthal@itba.edu.ar>
2022-07-17 21:14:59 -0300

    send images with cache

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/ImagesController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/ImagesController.java
@@ -9,7 +9,9 @@ import org.springframework.stereotype.Component;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.*;
+import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 @Component
@@ -28,11 +30,27 @@ public class ImagesController {
 
     @GET
     @Path("/{id}/")
-    @Produces(value = { MediaType.APPLICATION_JSON, })
-    public Response images (@PathParam("id") final Long id) {
+    @Produces(value = { MediaType.APPLICATION_OCTET_STREAM, })
+    public Response images (@PathParam("id") final Long id, @Context Request request) {
         final Image img = is.getImage(id).get(); //todo chequear que esta
-        return Response.ok(new GenericEntity<Image>(img){})
-                .build();
+        return sendWithCache(img.getImage(), request);
     }
 
+
+    private Response sendWithCache(final byte[] image, final Request request){
+        CacheControl cacheControl = new CacheControl();
+
+        cacheControl.setMaxAge(60000);
+        cacheControl.getCacheExtension().put("public","");
+        cacheControl.getCacheExtension().put("immutable","");
+        cacheControl.setNoTransform(true);
+        cacheControl.setMustRevalidate(true);
+
+        EntityTag tag = new EntityTag(Integer.toString(Arrays.hashCode(image)));
+        Response.ResponseBuilder responseBuilder = request.evaluatePreconditions(tag);
+        if(responseBuilder == null){
+            responseBuilder = Response.ok().entity(image).tag(tag);
+        }
+        return responseBuilder.cacheControl(cacheControl).build();
+    }
 }
