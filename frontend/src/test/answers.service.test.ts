06b50f3663030af9d5ed30c4206bc35743a7c227
szavalia <szavalia@extrimian.com>
2023-02-14 12:38:39 -0300

    Reworked answers tests to account for better axios error management

--- frontend/src/test/answers.service.test.ts
+++ frontend/src/test/answers.service.test.ts
@@ -1,64 +1,106 @@
-import { createAnswer, vote, deleteVote, getByOwner, unVerifyAnswer, verifyAnswer } from "../services/answers";
-import { api as mockApi} from "../services/api";
+import {
+  createAnswer,
+  vote,
+  deleteVote,
+  getByOwner,
+  unVerifyAnswer,
+  verifyAnswer,
+} from "../services/answers";
 import { NotFoundError } from "../models/HttpTypes";
+import { api } from "../services/api";
+import MockAdapter from "axios-mock-adapter";
 
 describe("AnswersService", () => {
-  
+  let mockAxios = new MockAdapter(api);
+
+  afterEach(() => {
+    mockAxios.reset();
+  });
+
   it("Should create answer when given a valid question ID", async () => {
-    const mockCreateAnswer = jest.fn().mockReturnValue({ status: 201 });
-    mockApi.post = mockCreateAnswer;
-    const answer = { body: "answer" };
+    const answer = "This is a test answer";
     const idQuestion = 1;
+
+    mockAxios.onPost(`/answers/${idQuestion}`).reply(201);
+    let spy = jest.spyOn(api, "post");    
+
     await createAnswer(answer, idQuestion);
-    expect(mockCreateAnswer).toHaveBeenCalledWith(`/answers/${idQuestion}`, {
+
+    expect(spy).toHaveBeenCalledWith(`/answers/${idQuestion}`, {
       body: answer,
     });
   });
 
+  
   it("Should throw error when creating answer with invalid question ID", async () => {
-    mockApi.post = jest.fn().mockReturnValue({ status: 404 });
-    const answer = { body: "answer" };
+    const answer = "answer";
     const idQuestion = -1;
-    await expect(createAnswer(answer, idQuestion)).rejects.toThrow(NotFoundError);
+
+    mockAxios.onPost(`/answers/${idQuestion}`).reply(404);
+
+    try {
+      await createAnswer(answer, idQuestion);
+    }
+    catch (e) {
+      expect(e).toBeInstanceOf(NotFoundError);
+    }
+
+    // await expect(createAnswer(answer, idQuestion)).rejects.toThrow(NotFoundError);
+    expect(jest.spyOn(api, "post")).toHaveBeenCalledWith(`/answers/${idQuestion}`, {
+      body: answer,
+    });
+    expect(jest.spyOn(api, "post")).toHaveBeenCalledTimes(1);
   });
 
   it("Should throw error when voting with invalid answer ID", async () => {
-    mockApi.put = jest.fn().mockReturnValue({ status: 404 });
     const idUser = 1;
     const id = -1;
     const voteValue = true;
+
+    mockAxios.onPut(`/answers/${id}/votes/users/${idUser}?vote=${voteValue}`).reply(404);
+
     await expect(vote(idUser, id, voteValue)).rejects.toThrow(NotFoundError);
   });
 
   it("Should throw error when deleting vote with invalid answer ID", async () => {
-    mockApi.delete = jest.fn().mockReturnValue({ status: 404 });
     const idUser = 1;
     const id = -1;
+
+    mockAxios.onDelete(`/answers/${id}/votes/users/${idUser}`).reply(404);
+
     await expect(deleteVote(idUser, id)).rejects.toThrow(NotFoundError);
   });
 
   it("Should throw error when verifying answer with invalid answer ID", async () => {
-    mockApi.post = jest.fn().mockReturnValue({ status: 404 });
     const id = -1;
-    await expect(verifyAnswer(id)).rejects.toThrow(NotFoundError);
+
+    mockAxios.onPost(`/answers/${id}/verify/`).reply(404);
+
+    try{
+      await verifyAnswer(id);
+    }
+    catch (e) {
+      expect(e).toBeInstanceOf(NotFoundError);
+    }
   });
 
   it("Should throw error when unverifying answer with invalid answer ID", async () => {
-    mockApi.delete = jest.fn().mockReturnValue({ status: 404 });
     const id = -1;
+
+    mockAxios.onDelete(`/answers/${id}/verify/`).reply(404);
+
     await expect(unVerifyAnswer(id)).rejects.toThrow(NotFoundError);
   });
 
-  it("Should throw error when listing answers with invalid question ID", async () => {
-    mockApi.get = jest.fn().mockReturnValue({ status: 404 });
-    
+  it("Should throw error when listing answers with invalid question ID", async () => {    
     let p = {
       requestorId: 1,
       page: 1,
     }
 
+    mockAxios.onGet(`/answers/${p.requestorId}?page=${p.page}`).reply(404);
+
     await expect(getByOwner(p)).rejects.toThrow(NotFoundError);
   });
-
 
 });
