6fed40dae41919374bba02e213e6e0d6bebfe36f
Valentino Riera Torraca <vriera@itba.edu.ar>
2023-07-16 16:25:42 -0300

    Exception mapping enabled

---
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/exceptions/IllegalArgumentExceptionMapper.java
@@ -0,0 +1,21 @@
+package ar.edu.itba.paw.webapp.exceptions;
+
+import ar.edu.itba.paw.webapp.controller.dto.SuccessDto;
+
+import javax.ws.rs.core.GenericEntity;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class IllegalArgumentExceptionMapper implements ExceptionMapper<IllegalArgumentException> {
+    @Override
+    public Response toResponse(IllegalArgumentException e) {
+        SuccessDto successDto = new SuccessDto();
+        successDto.setCode("error catcheadisimo");
+        return Response.status(Response.Status.BAD_REQUEST).entity(
+                new GenericEntity<SuccessDto>(
+                        successDto) {
+                }).build();
+    }
+}
