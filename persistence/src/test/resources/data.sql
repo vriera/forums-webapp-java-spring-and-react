286e49472c2c98c320b9b04dba94cb2a3716fcf0
Natali Lilienthal <nlilienthal@itba.edu.ar>
2024-02-04 13:39:58 -0300

    change test schema.sql and add dummy data

--- persistence/src/test/resources/data.sql
+++ persistence/src/test/resources/data.sql
@@ -4,3 +4,28 @@
 insert into users (user_id, username, email, password) values (1, 'User 1', 'user1@test.com', '123456');
 insert into users (user_id, username, email, password) values (2, 'User 2', 'user2@test.com', '223456');
 insert into users (user_id, username, email, password) values (3, 'User 3', 'user3@test.com', '323456');
+
+-- create 3 dummy communities
+insert into community (community_id, name, description, moderator_id) values (1, 'Community 1', 'Description 1', 1);
+insert into community (community_id, name, description, moderator_id) values (2, 'Community 2', 'Description 2', 2);
+insert into community (community_id, name, description, moderator_id) values (3, 'Community 3', 'Description 3', 3);
+
+-- create 3 dummy forums
+insert into forum (forum_id, name, community_id) values (1, 'Forum 1', 1);
+insert into forum (forum_id, name, community_id) values (2, 'Forum 2', 2);
+insert into forum (forum_id, name, community_id) values (3, 'Forum 3', 3);
+
+-- create 6 dummy questions
+insert into question (question_id, title, body, user_id, forum_id, time) values (1, 'Question 1', 'Body 1', 1, 1, '2021-01-01 00:00:00');
+insert into question (question_id, title, body, user_id, forum_id, time) values (2, 'Question 2', 'Body 2', 1, 1, '2021-01-01 00:00:00');
+insert into question (question_id, title, body, user_id, forum_id, time) values (3, 'Question 3', 'Body 3', 3, 3, '2021-01-01 00:00:00');
+insert into question (question_id, title, body, user_id, forum_id, time) values (4, 'Question 4', 'Body 4', 1, 1, '2021-01-01 00:00:00');
+insert into question (question_id, title, body, user_id, forum_id, time) values (5, 'Question 5', 'Body 5', 2, 2, '2021-01-01 00:00:00');
+insert into question (question_id, title, body, user_id, forum_id, time) values (6, 'Question 6', 'Body 6', 3, 3, '2021-01-01 00:00:00');
+
+-- create 4 dummy answers
+insert into answer (answer_id, body, verify, question_id, user_id, time) values (1, 'Answer 1', true, 1, 1, '2021-01-01 00:00:00');
+insert into answer (answer_id, body, verify, question_id, user_id, time) values (2, 'Answer 2', false, 1, 3, '2021-01-01 00:00:00');
+insert into answer (answer_id, body, verify, question_id, user_id, time) values (3, 'Answer 3', true, 2, 3, '2021-01-01 00:00:00');
+insert into answer (answer_id, body, verify, question_id, user_id, time) values (4, 'Answer 4', true, 3, 1, '2021-01-01 00:00:00');
+
--- persistence/src/test/resources/schema.sql
+++ persistence/src/test/resources/schema.sql
@@ -1,75 +1,75 @@
 CREATE TABLE IF NOT EXISTS users (
-                                     user_id INTEGER IDENTITY PRIMARY KEY,
-                                     username VARCHAR(250),
-                                     email VARCHAR(250) UNIQUE,
-                                     password VARCHAR(250)
+    user_id INTEGER IDENTITY PRIMARY KEY,
+    username VARCHAR(250),
+    email VARCHAR(250) UNIQUE,
+    password VARCHAR(250)
 );
 
 CREATE TABLE IF NOT EXISTS community(
-                                        community_id INTEGER IDENTITY PRIMARY KEY,
-                                        name VARCHAR(250) UNIQUE, --TODO: Renombrar nombres repetidos en producción
-                                        description LONGVARCHAR,
-                                        moderator_id INT,
-                                        FOREIGN KEY (moderator_id) REFERENCES users
+    community_id INTEGER IDENTITY PRIMARY KEY,
+    name VARCHAR(250) UNIQUE, --TODO: Renombrar nombres repetidos en producción
+    description LONGVARCHAR,
+    moderator_id INT,
+    FOREIGN KEY (moderator_id) REFERENCES users
 );
 
 CREATE TABLE IF NOT EXISTS  forum(
-                                     forum_id INTEGER IDENTITY PRIMARY KEY,
-                                     name VARCHAR(250),
-                                     community_id INT,
-                                     FOREIGN KEY (community_id) REFERENCES community
+    forum_id INTEGER IDENTITY PRIMARY KEY,
+    name VARCHAR(250),
+    community_id INT,
+    FOREIGN KEY (community_id) REFERENCES community
 );
 
+
+
 CREATE TABLE IF NOT EXISTS question (
-                                        question_id INTEGER IDENTITY PRIMARY KEY,
-                                        title VARCHAR(250),
-                                        body LONGVARCHAR,
-                                        user_id INT,
-                                        FOREIGN KEY (user_id) REFERENCES users,
-                                        forum_id INT,
-                                        FOREIGN KEY (forum_id) REFERENCES forum,
-                                        time TIMESTAMP DEFAULT NOW() NOT NULL
+    question_id INTEGER IDENTITY PRIMARY KEY,
+    title VARCHAR(250),
+    body LONGVARCHAR,
+    user_id INT,
+        FOREIGN KEY (user_id) REFERENCES users,
+    forum_id INT,
+        FOREIGN KEY (forum_id) REFERENCES forum,
+    time TIMESTAMP DEFAULT NOW() NOT NULL,
+    image_id INT,
+        FOREIGN KEY (image_id) references images
 );
 
 CREATE TABLE IF NOT EXISTS answer(
-                                     answer_id INTEGER IDENTITY primary key,
-                                     body LONGVARCHAR not null,
-                                     verify boolean,
-                                     question_id int not null,
-                                     foreign key (question_id) references question,
-                                     user_id int not null,
-                                     foreign key (user_id) references users
+    answer_id INTEGER IDENTITY primary key,
+    body LONGVARCHAR not null,
+    verify boolean,
+    question_id int not null,
+    foreign key (question_id) references question,
+    user_id int not null,
+    time TIMESTAMP DEFAULT NOW() NOT NULL,
+    foreign key (user_id) references users
 );
 
 CREATE TABLE IF NOT EXISTS answerVotes(
-                                          votes_id INTEGER IDENTITY primary key,
-                                          vote boolean,
-                                          answer_id INT,
-                                          foreign key (answer_id) references answer,
-                                          user_id INT,
-                                          foreign key (user_id) references users
-);
+    votes_id INTEGER IDENTITY primary key,
+    vote boolean,
+    answer_id INT,
+    foreign key (answer_id) references answer,
+    user_id INT,
+    foreign key (user_id) references users
+    );
 
 CREATE TABLE IF NOT EXISTS questionVotes(
-                                            votes_id INTEGER IDENTITY primary key,
-                                            vote boolean,
-                                            question_id INT,
-                                            foreign key (question_id) references question,
-                                            user_id INT,
-                                            foreign key (user_id) references users
+    votes_id INTEGER IDENTITY primary key,
+    vote boolean,
+    question_id INT,
+    foreign key (question_id) references question,
+    user_id INT,
+    foreign key (user_id) references users
 );
 
-CREATE TABLE IF NOT EXISTS access(
-                                     access_id INTEGER IDENTITY PRIMARY KEY,
-                                     community_id INT,
-                                     user_id INT,
-                                     access_type INT,
-                                     FOREIGN KEY (community_id) REFERENCES community,
-                                     FOREIGN KEY (user_id) REFERENCES users,
-                                     UNIQUE (community_id,user_id)
+CREATE TABLE IF NOT EXISTS access( /*TODO: modificar la BD actual en el deploy*/
+    access_id INTEGER IDENTITY PRIMARY KEY,
+    community_id INT,
+    user_id INT,
+    access_type INT,
+    FOREIGN KEY (community_id) REFERENCES community ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES users ON DELETE CASCADE,
+    UNIQUE (community_id,user_id)
 );
-
-
-
-
-
